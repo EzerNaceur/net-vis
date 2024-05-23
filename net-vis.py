310e33a392749568ab9ceadec11e54a86d1d52d4
N00ber17 <azernaceur17@gmail.com>
2024-05-23 23:05:47 +0100

    Finished the project

--- net-vis.py
+++ net-vis.py
@@ -2,18 +2,14 @@ import dpkt
 import socket
 import pygeoip
 
-import requests
+import sys
 
 gi = pygeoip.GeoIP('GeoLiteCity-data/GeoLiteCity.dat')
 
-def get_public_ip():
-    response = requests.get('https://api.ipify.org?format=json')
-    ip_address = response.json()['ip']
-    return ip_address
 
 def createKML(dstip):
     dst = gi.record_by_name(dstip)
-    srcip = get_public_ip()
+    srcip = sys.argv[1]
     src = gi.record_by_name(srcip)
     try:
         dstLong = dst['longitude']
@@ -27,17 +23,16 @@ def createKML(dstip):
             '<tessellate>1</tessellate>\n'
             '<styleUrl>#customStyle</styleUrl>\n'
             '<LineString>\n'
-            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+            '<coordinates> %6f,%6f\n%6f,%6f </coordinates>\n'
             '</LineString>\n'
             '</Placemark>\n'
             )%(dstip, dstLong, dstLat, srcLong, srcLat)
         return kml
     except:
-        print("Error while creating kml")
+        return ''
             
 
-
-def plotIPs(pcap):
+def pltips(pcap):
     kmlpts = ''
     for (ts, buf) in pcap:
         try:
@@ -45,24 +40,31 @@ def plotIPs(pcap):
             ip = eth.data
             src = socket.inet_ntoa(ip.src)
             dest = socket.inet_ntoa(ip.dst)
-            KML = createKML(dest, src)
+            KML = createKML(dest)
             kmlpts = kmlpts + KML
         except:
-            print("There has been an error!/n")
+            pass
     return kmlpts
+
 
 def main():
     file = open('tcpdump.pcap', 'rb')
     pcap = dpkt.pcap.Reader(file)
     kmlhead = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'\
-              '<Style id="customStyle">' \
-              '<width>1<width>' \
-              '<color>1B85B</color>' \
-              '</LineStyle>' \
-              '</Style>'
+              '<Style id="customStyle">\n' \
+              '<LineStyle>\n' \
+              '<width>1</width>\n' \
+              '<color>7dff0000</color>\n' \
+              '</LineStyle>\n' \
+              '</Style>\n'
     kmlfoot = '</Document>\n</kml>\n'
-    kmldoc = kmlhead + plotIPs(pcap)+kmlfoot
-    print(kmldoc)
-    
+    kmldoc = kmlhead + pltips(pcap) + kmlfoot
+    with open('result.kml', 'w') as file:
+        file.write(kmldoc)
+
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        print('[x] Please specify an ip address: python net-vis.py ip_address\n')
+    else:
+        main()
